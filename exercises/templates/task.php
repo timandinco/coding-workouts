7aa1632e96b02f2f1fd3bbb79ccf37bc48e9f3d8
Tim Anderson <timand_2000@hotmail.com>
2026-06-26 22:27:25 -0600

    Updates

---
+++ exercises/templates/task.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP</title>
+</head>
+<body>
+    <h1></h1>
+    <p>
+
+    </p>
+<ul>
+<li><strong>Input:</strong></li>
+
+</ul>
+
+
+    <h4>PHP Code:</h4>
+    <pre>
+
+    </pre>
+
+    <h4>Output:</h4>
+    <?php
+    
+    ?>
+
+</body>
